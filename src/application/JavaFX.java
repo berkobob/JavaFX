2eaa5bd4d93c374a3120cc29c1f46fdb3d354430
berkobob <alever@omniis.com>
2016-12-19 12:43:53 +0000

    Switching scenes

--- src/application/JavaFX.java
+++ src/application/JavaFX.java
@@ -5,12 +5,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFX extends Application {
 
-	Button button;
+	Stage window;
+	Scene scene1, scene2;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -18,23 +21,27 @@ public class JavaFX extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Title of the Window");
+		window = primaryStage;
 		
-		button = new Button("Click me");
-		button.setOnAction(e -> System.out.println("Button pressed"));
+		Label label1 = new Label("Welcome to switching between scenes");
+		Button button1 = new Button("Go to scene 2");
+		button1.setOnAction(e-> window.setScene(scene2));
 		
-		Button button2 = new Button("Prints two lines");
-		button2.setOnAction(e -> {
-			System.out.println("Line 1");
-			System.out.println("Line 2");
-		});
+		//Layout 1 - children are laid out in a vertical column
+		VBox layout1 = new VBox(20); // 20 means 20px between children
+		layout1.getChildren().addAll(label1, button1);
+		scene1 = new Scene(layout1, 200, 200);
 		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
-		layout.getChildren().add(button2);
+		Button button2 = new Button("Go to scene 1");
+		button2.setOnAction(e-> window.setScene(scene1));
 		
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		//Layout 2
+		StackPane layout2 = new StackPane();
+		layout2.getChildren().add(button2);
+		scene2 = new Scene(layout2, 400,400);
+		
+		window.setScene(scene1);
+		window.setTitle("HEre is my title");
+		window.show();
 	}
 }
