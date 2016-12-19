162f83d0950355154f4c339a6fecaabe859a416d
berkobob <alever@omniis.com>
2016-12-19 12:16:33 +0000

    Events calling a named object

---
+++ src/application/JavaFX.java
@@ -0,0 +1,41 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFX extends Application implements EventHandler<ActionEvent>{
+
+	Button button;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Title of the Window");
+		
+		button = new Button("Click me");
+		button.setOnAction(this);
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		System.out.println("Figured it out!!");
+		System.out.println(arg0.getSource());
+		System.out.println(arg0.getEventType());
+	}
+
+}
